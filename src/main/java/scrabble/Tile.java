d1670fb2542b220752a533e640a0eafc9e6763e8
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-01-28 20:41:20 +0000

    Add helper methods to Tile [WIP] No code just skeleton with comments.

--- src/main/java/scrabble/Tile.java
+++ src/main/java/scrabble/Tile.java
@@ -27,5 +27,38 @@ public enum Tile {
     W,
     X,
     Y,
-    Z
+    Z;
+
+    /**
+     * Gets the value associated with this tile.
+     * This is the score given to the player when they use a tile of this type.
+     *
+     * @return the value of <code>this</code>
+     */
+    public int getValue() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Gets the character associated with this tile.
+     * This is either an uppercase character or a <code>'?'</code> in the case of the BLANK tile.
+     *
+     * @return the symbol for <code>this</code>
+     */
+    public char getSymbol() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Gets the starting number of this kind of tile that should be placed in the {@link scrabble.Pool} at the start of
+     * a game.
+     *
+     * @return how many of this tile should be put in the pool
+     */
+    public int getDefaultStartingCount() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
 }
