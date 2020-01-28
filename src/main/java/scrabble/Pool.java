09ec6cebe6424cc785761bdfda821f5992d3918f
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-01-28 14:07:54 +0000

    Pool skeleton

--- src/main/java/scrabble/Pool.java
+++ src/main/java/scrabble/Pool.java
@@ -1,4 +1,31 @@
 package scrabble;
 
 public class Pool {
+
+    // TODO: implement fields
+
+    public Pool() {
+        // TODO: implement code
+    }
+
+    public void reset() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public int size() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isEmpty() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public Tile takeTile() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
 }
