c40df9aa5741a5e9f5200d022f99123c6e907547
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-01-28 14:58:33 +0000

    Player class skeleton

--- src/main/java/scrabble/Player.java
+++ src/main/java/scrabble/Player.java
@@ -1,4 +1,40 @@
 package scrabble;
 
 public class Player {
+	
+	 // TODO: implement fields
+
+    public Player(String name) {
+        // TODO: implement code
+    }
+
+    public void reset() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public int increaseScore() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+    
+    public int getScore() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public Frame getFrame() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+
+    public void setName(String name) {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
+    public String getName() {
+        // TODO: implement code
+        throw new UnsupportedOperationException();
+    }
 }
+
