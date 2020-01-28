086aafbd86f8d397fb2cecdcd67feb553d9d3c72
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-01-28 20:34:27 +0000

    Document expected behaviour for Pool
    Should assist with making tests.

--- src/main/java/scrabble/Pool.java
+++ src/main/java/scrabble/Pool.java
@@ -1,5 +1,10 @@
 package scrabble;
 
+/**
+ * Pool acts as a bag of {@link scrabble.Tile} that the players can take tiles from at random.
+ *
+ * When instantiated, will fill itself with correct number of each tile.
+ */
 public class Pool {
 
     // TODO: implement fields
@@ -8,21 +13,38 @@ public class Pool {
         // TODO: implement code
     }
 
+    /**
+     * Resets pool to original state by refilling with correct number of each tile.
+     */
     public void reset() {
         // TODO: implement code
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @return the number of tiles currently in the pool
+     */
     public int size() {
         // TODO: implement code
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @return whether the pool is empty or not
+     */
     public boolean isEmpty() {
         // TODO: implement code
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Takes a single {@link scrabble.Tile} from the pool at random.
+     * The size of the pool will decrease by 1.
+     *
+     * @throws java.util.NoSuchElementException if pool is empty.
+     *
+     * @return the tile taken from the pool
+     */
     public Tile takeTile() {
         // TODO: implement code
         throw new UnsupportedOperationException();
