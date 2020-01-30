e56e003caba9c25e5f4f8f636bd91487b511a13d
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-01-30 13:20:57 +0000

    Updates to Player class

--- src/main/java/scrabble/Player.java
+++ src/main/java/scrabble/Player.java
@@ -3,38 +3,50 @@ package scrabble;
 public class Player {
 	
 	 // TODO: implement fields
+	public String name;
+	public int score;
+	public Frame frame;
 
     public Player(String name) {
         // TODO: implement code
     }
 
-    public void reset() {
-        // TODO: implement code
+    public Player reset(String name) {
+        // TODO: implement code 
+    	this.name = name;
+    	this.score = 0;
         throw new UnsupportedOperationException();
     }
 
-    public int increaseScore() {
+    public void increaseScore(int amount) {
         // TODO: implement code
-        throw new UnsupportedOperationException();
+    	if(amount < 0) {
+    		throw new UnsupportedOperationException();
+    	}
+    	this.score += amount;
     }
     
     public int getScore() {
         // TODO: implement code
-        throw new UnsupportedOperationException();
+        // new UnsupportedOperationException();
+    	return this.score;
     }
 
     public Frame getFrame() {
         // TODO: implement code
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+    	return this.frame;
     }
 
     public void setName(String name) {
         // TODO: implement code
+    	this.name = name;
         throw new UnsupportedOperationException();
     }
     public String getName() {
         // TODO: implement code
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        return this.name;
     }
 }
 
