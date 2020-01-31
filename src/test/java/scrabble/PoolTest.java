ba6a7bb8f2f57096e2550fbc93b7c586e808330c
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-01-31 09:55:36 +0000

    pool size test

--- src/test/java/scrabble/PoolTest.java
+++ src/test/java/scrabble/PoolTest.java
@@ -2,6 +2,50 @@ package scrabble;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 class PoolTest {
+	
+	
+	
+    @Test
+    void reset() {
+    	Pool pool = new Pool();
+    	assertEquals(100, pool.size());
+    }
+
+    @Test
+    void size() {
+    	Pool pool = new Pool();
+    	assertEquals(100, pool.size());
+    	
+    	pool.takeTile();
+    	assertEquals(99, pool.size());
+    	
+    	for(int i = 98; i >=0; i--) {
+    		pool.takeTile();
+    		assertEquals(i, pool.size());
+    	}
+    	
+    	assertEquals(0, pool.size());
+    }
+    
+    @Test
+    void testInitialContents() {
+    	
+    }
+
+    @Test
+    void isEmpty() {
+    	Pool pool = new Pool();
+    	assertTrue(pool.isEmpty());
+    }
+
+    @Test
+    void takeTile() {
+    }
 
 }
