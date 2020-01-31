289b2ecd6811b52cc88a05fca6313c60f2b7adb5
jayengracio <john.engracio@ucdconnect.ie>
2020-01-31 15:11:22 +0000

    Update PlayerTest.java
    Some test case implementations for the class Player

--- src/test/java/scrabble/PlayerTest.java
+++ src/test/java/scrabble/PlayerTest.java
@@ -1,7 +1,40 @@
 package scrabble;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    @Test
+    void testName(){
+        Player newPlayer = new Player("John");
+
+        assertEquals("John", newPlayer.getName());
+
+        newPlayer.setName("Doe");
+
+        assertEquals("Doe", newPlayer.getName());
+    }
+
+    @Test
+    void testScore(){
+        Player newPlayer = new Player("James");
+
+        assertEquals(0, newPlayer.getScore());
+
+        newPlayer.increaseScore(10);
+        assertEquals(10, newPlayer.getScore());
+    }
+
+    @Test
+    void testReset(){
+        Player newPlayer = new Player("Steven");
+
+        newPlayer.increaseScore(25);
+        assertEquals(25, newPlayer.getScore());
+
+        newPlayer.reset();
+        assertEquals(0, newPlayer.getScore());
+    }
 }
