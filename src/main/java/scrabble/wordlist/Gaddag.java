d33418cf0b2ed5edab377d59816c30ccdfcb0a06
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-01-31 18:07:27 +0000

    Return list of Join instead of strings for Gaddag complete

--- src/main/java/scrabble/wordlist/Gaddag.java
+++ src/main/java/scrabble/wordlist/Gaddag.java
@@ -1,5 +1,6 @@
 package scrabble.wordlist;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
@@ -19,7 +20,7 @@ public class Gaddag {
             String left = word.substring(0, i);
             String right = word.substring(i);
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder(left.length() + right.length() + 1);
             sb.append(left).reverse();
 
             if (!right.isEmpty()) {
@@ -30,15 +31,48 @@ public class Gaddag {
         }
     }
 
-    public List<String> complete(String s) {
+    public class Join {
+        public String left, middle, right;
+
+        public Join(String left, String middle, String right) {
+            this.left = left;
+            this.middle = middle;
+            this.right = right;
+        }
+
+        public Join(String middle, String keyRight) {
+            this.middle = middle;
+            int i = keyRight.indexOf('+');
+            if (i != -1) {
+                this.left = new StringBuilder(keyRight.substring(0, i)).reverse().toString();
+                this.right = keyRight.substring(i+1);
+            } else {
+                this.left = new StringBuilder(keyRight).reverse().toString();
+                this.right = "";
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Join[%s,%s,%s]", left, middle, right);
+        }
+    }
+
+    public List<Join> complete(String s) {
         String sReversed = new StringBuilder(s).reverse().toString();
 
         try {
             Trie child = rootTrie.get(sReversed);
 
-            return child.collectAll();
+            List<String> list = child.collectAll();
+
+            List<Join> joinList = new ArrayList<>(list.size());
+            for (String rest : list) {
+                joinList.add(new Join(s, rest));
+            }
+            return joinList;
         } catch (NoSuchElementException e) {
-            return null;
+            return new ArrayList<>();
         }
     }
 
