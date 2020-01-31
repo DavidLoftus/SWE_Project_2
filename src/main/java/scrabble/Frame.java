930d96d2ef9f3af5ac264b5e5e223121003a8cd1
jayengracio <john.engracio@ucdconnect.ie>
2020-01-31 11:47:07 +0000

    Update Frame.java
    Implementations to Frame

--- src/main/java/scrabble/Frame.java
+++ src/main/java/scrabble/Frame.java
@@ -1,4 +1,44 @@
 package scrabble;
 
+import java.util.List;
+
 public class Frame {
+
+    private List<Tile> tiles;
+
+    public boolean inFrame(Tile letter)
+    {
+        for (Tile i : tiles) {
+            if (i == letter) {
+                return true;
+            }
+            //else return false;
+        }
+        return false;
+    }
+
+    public void removeLetter(Tile letter)
+    {
+        if (tiles.contains(letter)) {
+            tiles.remove(letter);
+        }
+        else throw new IllegalArgumentException(letter.toString() + " does not exist in your frame.");
+    }
+
+    public void refill(Pool pool){
+        int index = 0;
+        while (index <= 7){
+            // TODO: finish code
+            Tile newTile = pool.takeTile();
+        }
+    }
+
+    public boolean isEmpty(){
+        return tiles.isEmpty();
+    }
+
+    public String toString(){
+        // TODO: implement code
+        return ("Your frame consists of: ");
+    }
 }
