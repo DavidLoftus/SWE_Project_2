33357493f78006b7f8954b203021b2cf1cf4af28
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-02-04 12:27:36 +0000

    Reformat code

--- src/main/java/scrabble/Player.java
+++ src/main/java/scrabble/Player.java
@@ -1,61 +1,55 @@
 package scrabble;
 
 public class Player {
-	
-	private String name;
-	private int score = 0;
-	private Frame frame = new Frame();
 
-	/**
-	 * sets the players name
-	 * @param name
-	 */
-    public Player(String name) {
-    	this.name = name;
-    }
+    private String name;
+    private int score = 0;
+    private Frame frame = new Frame();
+
     /**
-     * sets the players scores back to zero and 
-     * gives them a new frame. 
-     */
-    public void reset() {
-    	this.score = 0;
-    	this.frame = new Frame();
-    }
-    /**
-     * this increases the score of a player by amount.
-     * @param amount
-     */
-    public void increaseScore(int amount) {
-    	if(amount < 0) {
-    		throw new IllegalArgumentException("amount cant be negative");
-    	}
-    	this.score += amount;
-    }
-    
-    /**
-     * @return the current score
-     */
-    public int getScore() {
-    	return this.score;
-    }
-    
-    /**
-     * @return the frame of a player
-     */
-    public Frame getFrame() {
-    	return this.frame;
-    }
-    /**
+     * sets the players name
+     *
      * @param name
      */
-    public void setName(String name) {
-    	this.name = name;
+    public Player(String name) {
+        this.name = name;
+    }
+    /** Resets the players scores back to zero and gives them a new frame. */
+    public void reset() {
+        this.score = 0;
+        this.frame = new Frame();
     }
     /**
-     * @return the name of the player
+     * Increases the score of a player by amount.
+     *
+     * @param amount to increase score by.
      */
+    public void increaseScore(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("amount cant be negative");
+        }
+        this.score += amount;
+    }
+
+    /** @return the player's current score */
+    public int getScore() {
+        return this.score;
+    }
+
+    /** @return the frame of the player */
+    public Frame getFrame() {
+        return this.frame;
+    }
+    /**
+     * Sets the player's name.
+     *
+     * @param name is to be the new name of the player
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /** @return the name of the player */
     public String getName() {
         return this.name;
     }
 }
-
--- src/test/java/scrabble/PoolTest.java
+++ src/test/java/scrabble/PoolTest.java
@@ -3,84 +3,82 @@ package scrabble;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.NoSuchElementException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PoolTest {
-	
-	private Pool pool;
-	
-	@BeforeEach
-	void initPool() {
-		pool = new Pool();
-	}
-	
+
+    private Pool pool;
+
+    @BeforeEach
+    void initPool() {
+        pool = new Pool();
+    }
+
     @Test
     void reset() {
-    	pool.takeTile();
-    	pool.reset();
-    	
-    	assertEquals(100, pool.size());
-    	
-    	testInitialContents();
+        pool.takeTile();
+        pool.reset();
+
+        assertEquals(100, pool.size());
+
+        testInitialContents();
     }
 
     @Test
     void size() {
-    	assertEquals(100, pool.size());
-    	
-    	pool.takeTile();
-    	assertEquals(99, pool.size());
-    	
-    	for(int i = 98; i >=0; i--) {
-    		pool.takeTile();
-    		assertEquals(i, pool.size());
-    	}
-    	
-    	assertEquals(0, pool.size());
+        assertEquals(100, pool.size());
+
+        pool.takeTile();
+        assertEquals(99, pool.size());
+
+        for (int i = 98; i >= 0; i--) {
+            pool.takeTile();
+            assertEquals(i, pool.size());
+        }
+
+        assertEquals(0, pool.size());
     }
-    
+
     @Test
     void testInitialContents() {
-    	int[] counts = new int[Tile.values().length];
-    	
-    	while(!pool.isEmpty()) {
-    		Tile tile = pool.takeTile();
-    		counts[tile.ordinal()]++;
-    	}
-    	
-    	for (Tile tile : Tile.values()) {
-    		assertEquals(tile.getStartingCount(), counts[tile.ordinal()]);
-    	}
+        int[] counts = new int[Tile.values().length];
+
+        while (!pool.isEmpty()) {
+            Tile tile = pool.takeTile();
+            counts[tile.ordinal()]++;
+        }
+
+        for (Tile tile : Tile.values()) {
+            assertEquals(tile.getStartingCount(), counts[tile.ordinal()]);
+        }
     }
 
     @Test
     void isEmpty() {
-    	assertFalse(pool.isEmpty());
-    	
-    	pool.takeTile();
-    	assertFalse(pool.isEmpty());
-    	
-    	for(int i = 98; i > 0; i--) {
-    		pool.takeTile();
-    		assertFalse(pool.isEmpty());
-    	}
-    	
-    	pool.takeTile();
-    	assertTrue(pool.isEmpty());
+        assertFalse(pool.isEmpty());
+
+        pool.takeTile();
+        assertFalse(pool.isEmpty());
+
+        for (int i = 98; i > 0; i--) {
+            pool.takeTile();
+            assertFalse(pool.isEmpty());
+        }
+
+        pool.takeTile();
+        assertTrue(pool.isEmpty());
     }
 
     @Test
     void takeTile() {
-    	pool.takeTile();
-    	
-    	for(int i = 98; i >= 0; i--) {
-    		pool.takeTile();
-    	}
-    	
-    	assertTrue(pool.isEmpty());
-    	assertThrows(NoSuchElementException.class, pool::takeTile);  	
-    }
+        pool.takeTile();
 
+        for (int i = 98; i >= 0; i--) {
+            pool.takeTile();
+        }
+
+        assertTrue(pool.isEmpty());
+        assertThrows(NoSuchElementException.class, pool::takeTile);
+    }
 }
