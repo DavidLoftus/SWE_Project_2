45036a27e9e1d72c980e88f688d95c1eb365c286
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-02-07 10:28:31 +0000

    Added test for toString in PlaterTest

--- src/test/java/scrabble/PlayerTest.java
+++ src/test/java/scrabble/PlayerTest.java
@@ -37,4 +37,23 @@ class PlayerTest {
         newPlayer.reset();
         assertEquals(0, newPlayer.getScore());
     }
+    
+    @Test
+    void testToString() {
+    	Player newPlayer = new Player("David");
+    	assertEquals("David (0) [       ]", newPlayer.toString());
+    	newPlayer.increaseScore(30);
+    	assertEquals("David (30) [       ]", newPlayer.toString());
+    	FrameTest.FakePool pool = new FrameTest.FakePool();
+    	
+    	pool.add(Tile.BLANK);
+        pool.add(Tile.A);
+        pool.add(Tile.B);
+        pool.add(Tile.C);
+        pool.add(Tile.G);
+        pool.add(Tile.E);
+        pool.add(Tile.BLANK);
+        
+        assertEquals("David (30) [BLANK A B C G E BLANK]", newPlayer.toString());
+    }
 }
