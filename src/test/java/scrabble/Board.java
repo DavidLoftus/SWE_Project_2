94f0286d5688b1d201034f3bdd8dd5057ba74bbd
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-02-12 14:14:54 +0000

    Board skeleton

---
+++ src/test/java/scrabble/Board.java
@@ -0,0 +1,23 @@
+package scrabble;
+
+public class Board {
+
+	public void resetBoard() {
+		//TODO
+	}
+	public void setTilePos(Tile tile) {
+		//TODO
+	}
+	//not void
+	public void getTilePos(){
+		//TODO
+	}
+	//do we want this to place a tile or a hole word
+	public void placeOnBoard(Tile tile) {
+		//TODO
+	}
+	public void printBoard(Board board) {
+		//TODO
+	}
+	
+}
