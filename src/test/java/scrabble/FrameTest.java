d23248999e9a1d2d99a942dd8d74033d4a036fd2
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-02-14 23:02:36 +0000

    New imports in FrameTest

--- src/test/java/scrabble/FrameTest.java
+++ src/test/java/scrabble/FrameTest.java
@@ -3,7 +3,10 @@ package scrabble;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FrameTest {
     @Test
