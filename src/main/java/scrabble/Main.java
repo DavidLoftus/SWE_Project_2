d31b4172220a1ae5eef1841082f402c4914dff9f
jayengracio <john.engracio@ucdconnect.ie>
2020-02-18 20:16:14 +0000

    some implementations
    - printing out board in Main
    - getLetterAt & setTile implementations in Board
    - placeholders for WordPlacement

--- src/main/java/scrabble/Main.java
+++ src/main/java/scrabble/Main.java
@@ -3,8 +3,9 @@ package scrabble;
 public class Main {
 
     public static void main(String[] args) {
+        Board board = new Board();
         Pool pool = new Pool();
-        Player player = new Player("Yams");
+        Player player = new Player("Player");
 
         System.out.println(player);
         System.out.println(pool);
@@ -18,5 +19,12 @@ public class Main {
 
         player.reset();
         System.out.println(player);
+
+        System.out.println("");
+        board.printBoard();
+        System.out.println("\n");
+        board.setTile(7,7, Tile.X);
+        board.printBoard();
+        System.out.println("\nLetter: " + board.getLetterAt(7, 7));
     }
 }
--- src/main/java/scrabble/WordPlacement.java
+++ src/main/java/scrabble/WordPlacement.java
@@ -1,3 +1,25 @@
 package scrabble;
 
-public class WordPlacement {}
+
+public class WordPlacement {
+
+    // placeholder
+    public Tile letterToPlace(Tile tile){
+        return tile;
+    }
+
+    // placeholder
+    public boolean checkTile(Player player, Tile tile) {
+        if (player.getFrame().getTiles().contains(letterToPlace(tile))){
+            return true;
+        }
+
+        else return false;
+    }
+
+    /*public void placeLetter(Tile tile, int i, int j) {
+        if (checkTile() == true){  }
+    }*/
+
+
+}
