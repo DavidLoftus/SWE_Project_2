779c209d3e289fec4d9a4d96831707346c85cec2
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-02-19 15:21:48 +0000

    added boarders around squares in the printBoard

--- src/main/java/scrabble/Board.java
+++ src/main/java/scrabble/Board.java
@@ -73,11 +73,13 @@ public class Board {
     }
 
     public void printBoard() {
-		for (int i = 0; i < 15; ++i) {
+        System.out.println("+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+");
+        for (int i = 0; i < 15; ++i) {
+            System.out.print("|");
             for (int j = 0; j < 15; ++j) {
-            	System.out.print(grid[i][j].toString());
+                System.out.print(grid[i][j].toString() + "|");
             }
-            System.out.println();
+            System.out.println("\n+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+");
         }
     }
 }
