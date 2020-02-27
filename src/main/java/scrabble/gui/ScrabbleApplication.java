cf623065345a60d038573388b4352eef7c1f064e
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-02-27 13:41:48 +0000

    JavaFX gradle config

---
+++ src/main/java/scrabble/gui/ScrabbleApplication.java
@@ -0,0 +1,11 @@
+package scrabble.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScrabbleApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+}
