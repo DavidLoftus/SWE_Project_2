ec93a94993bf3ab5f465ef11441a9dc5f16ca5d9
GitHub Action <action@github.com>
2020-03-03 23:03:28 +0000

    Automatic javadoc update triggered passing tests after push of commit: d55094e0f8aa8406e35c5f3b08c9bd7c87852996

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"scrabble"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"scrabble"},{"l":"scrabble.exceptions"},{"l":"scrabble.gui"}]
