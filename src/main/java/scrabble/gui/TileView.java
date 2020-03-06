84b95a8b3972fa1361f3206dbb2115f3d9679fe2
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-03-06 10:37:05 +0000

    Yo we got that tileview all running and shiz

--- src/main/java/scrabble/gui/TileView.java
+++ src/main/java/scrabble/gui/TileView.java
@@ -1,5 +1,27 @@
 package scrabble.gui;
 
-import javafx.scene.layout.StackPane;
+import javafx.beans.NamedArg;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import scrabble.Tile;
 
-public class TileView extends StackPane {}
+public class TileView extends Label {
+
+    private Tile tile;
+
+    public TileView(@NamedArg("tile") Tile tile) {
+        this.tile = tile;
+
+        setBackground(
+                new Background(
+                        new BackgroundFill(Color.GOLDENROD, new CornerRadii(1.0), Insets.EMPTY)));
+        setText(Character.toString(tile.getLetter()));
+
+        setAlignment(Pos.CENTER);
+    }
+}
