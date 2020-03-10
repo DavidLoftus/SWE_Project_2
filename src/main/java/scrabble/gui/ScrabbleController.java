38dfee26c817d4bce04f0e0b7c2883085b0e533d
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-03-10 23:53:02 +0000

    Update fxml

--- src/main/java/scrabble/gui/ScrabbleController.java
+++ src/main/java/scrabble/gui/ScrabbleController.java
@@ -1,3 +1,10 @@
 package scrabble.gui;
 
-public class ScrabbleController {}
+import javafx.fxml.FXML;
+
+public class ScrabbleController {
+
+    @FXML public BoardView boardGrid;
+
+    @FXML public FrameView frame;
+}
