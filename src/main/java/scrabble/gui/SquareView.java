6fdf170b662f401ce5aef5ce1a59952ae040c30d
jayengracio <john.engracio@ucdconnect.ie>
2020-03-10 21:35:19 +0000

    Drawing tile in SquareView

--- src/main/java/scrabble/gui/SquareView.java
+++ src/main/java/scrabble/gui/SquareView.java
@@ -1,21 +1,44 @@
 package scrabble.gui;
 
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import scrabble.Square;
 
 public class SquareView extends StackPane {
+
     private Square square;
 
-    SquareView(Square square) {
+    private Label modifierLabel;
+
+    public SquareView(Square square) {
         this.square = square;
 
+        Square.Modifier modifier = square.getModifier();
+
+        setPrefSize(50, 50);
+
         setBackground(
-                new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
+                new Background(
+                        new BackgroundFill(
+                                getColourForModifier(modifier),
+                                new CornerRadii(1.0),
+                                new Insets(1.0))));
+
+        modifierLabel = getLabelForModifier(modifier);
+
+        setAlignment(Pos.CENTER);
+
+        updateTile();
     }
 
     public Color getColourForModifier(Square.Modifier modifier) {
@@ -37,7 +60,48 @@ public class SquareView extends StackPane {
         }
     }
 
-    public void setSquare(Square square) {
-        this.square = square;
+    public Label getLabelForModifier(Square.Modifier modifier) {
+        Label label = new Label();
+        label.setWrapText(true);
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setFont(new Font(10.0));
+        switch (modifier) {
+            case DOUBLE_WORD:
+                label.setText("Double Word");
+                break;
+            case DOUBLE_LETTER:
+                label.setText("Double Letter");
+                break;
+            case TRIPLE_WORD:
+                label.setText("Triple Word");
+                break;
+            case TRIPLE_LETTER:
+                label.setText("Triple Letter");
+                break;
+            case NORMAL:
+                break;
+            case STAR:
+                label.setText("*");
+                break;
+        }
+        return label;
+    }
+
+    public void updateTile() {
+        ObservableList<Node> children = getChildren();
+        if (square.isEmpty()) {
+            children.setAll(modifierLabel);
+            return;
+        }
+        if (!children.isEmpty()) {
+            Node child = children.get(0);
+            if (child instanceof TileView) {
+                TileView tileView = (TileView) child;
+                if (tileView.getTile() == square.getTile()) {
+                    return;
+                }
+            }
+        }
+        getChildren().setAll(modifierLabel, new TileView(square.getTile()));
     }
 }
