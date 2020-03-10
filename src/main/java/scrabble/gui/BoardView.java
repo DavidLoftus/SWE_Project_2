7b814d12648a85a4f4e5031ad79e6bfd4a958760
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-03-10 23:53:57 +0000

    BoardView class

--- src/main/java/scrabble/gui/BoardView.java
+++ src/main/java/scrabble/gui/BoardView.java
@@ -1,4 +1,53 @@
-package scrabble;
+package scrabble.gui;
 
-public class BoardView {
+import java.util.Collections;
+import javafx.scene.Node;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import scrabble.Board;
+import scrabble.BoardPos;
+
+public class BoardView extends GridPane {
+
+    private Board board = null;
+
+    public BoardView() {
+
+        widthProperty().addListener((__, ___, w) -> setHeight(w.doubleValue()));
+        heightProperty().addListener((__, ___, h) -> setWidth(h.doubleValue()));
+
+        RowConstraints rowConstraints = new RowConstraints();
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+
+        getRowConstraints().setAll(Collections.nCopies(15, rowConstraints));
+        getColumnConstraints().setAll(Collections.nCopies(15, columnConstraints));
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+        updateGridBase();
+        updateGridTiles();
+
+        System.out.printf("%f, %f\n", getWidth(), getHeight());
+    }
+
+    private void updateGridBase() {
+        assert board != null;
+
+        for (int i = 0; i < 15; ++i) {
+            for (int j = 0; j < 15; ++j) {
+                BoardPos pos = new BoardPos(i, j);
+                add(board.getSquareViewAt(pos), i, j);
+            }
+        }
+    }
+
+    public void updateGridTiles() {
+        assert board != null;
+        for (Node child : getChildrenUnmodifiable()) {
+            SquareView squareView = (SquareView) child;
+            squareView.updateTile();
+        }
+    }
 }
--- src/main/java/scrabble/gui/CommandPanel.java
+++ src/main/java/scrabble/gui/CommandPanel.java
@@ -1,4 +1,37 @@
 package scrabble.gui;
 
-public class CommandPanel {
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+public class CommandPanel extends VBox {
+
+    public CommandPanel() {
+
+        TextFlow textContainer = new TextFlow();
+        textContainer.setBackground(
+                new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        ScrollPane scrollPane = new ScrollPane(textContainer);
+        scrollPane.setVvalue(1.0);
+
+        setVgrow(textContainer, Priority.SOMETIMES);
+
+        TextField inputField = new TextField();
+
+        inputField.setOnAction(
+                evt -> {
+                    Text text = new Text(inputField.getText() + "\n");
+                    text.setStyle("-fx-text-fill: #ff282f;");
+                    textContainer.getChildren().add(text);
+                    inputField.clear();
+                    scrollPane.setVvalue(1.0);
+                });
+
+        getChildren().setAll(scrollPane, inputField);
+    }
 }
