230ed446b1ce06e83476ea16c5f95937850a0f1f
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-03-12 12:27:13 +0000

    added print method to CommandPanel

--- src/main/java/scrabble/gui/CommandPanel.java
+++ src/main/java/scrabble/gui/CommandPanel.java
@@ -9,29 +9,37 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class CommandPanel extends VBox {
+	
+	TextFlow textContainer;
+	ScrollPane scrollPane;
+    TextField inputField;
 
     public CommandPanel() {
-
-        TextFlow textContainer = new TextFlow();
+    	
+    	textContainer = new TextFlow();
         textContainer.setBackground(
                 new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        ScrollPane scrollPane = new ScrollPane(textContainer);
+        scrollPane = new ScrollPane(textContainer);
         scrollPane.setVvalue(1.0);
-
+        
         setVgrow(textContainer, Priority.SOMETIMES);
 
         TextField inputField = new TextField();
 
         inputField.setOnAction(
                 evt -> {
-                    Text text = new Text(inputField.getText() + "\n");
-                    text.setStyle("-fx-text-fill: #ff282f;");
-                    textContainer.getChildren().add(text);
-                    inputField.clear();
-                    scrollPane.setVvalue(1.0);
+                	print(inputField.getText());
                 });
 
         getChildren().setAll(scrollPane, inputField);
+    }
+    
+    public void print(String str) {
+    	Text text = new Text(str + "\n");
+        text.setStyle("-fx-text-fill: #ff282f;");
+        textContainer.getChildren().add(text);
+        inputField.clear();
+        scrollPane.setVvalue(1.0);
     }
 }
