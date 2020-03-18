761f8ffc4e4417b4ef24662f04ff7b35b9c3253b
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-03-18 20:25:32 +0000

    InputCommand valueOf updated

--- src/main/java/scrabble/input/InputCommand.java
+++ src/main/java/scrabble/input/InputCommand.java
@@ -1,14 +1,27 @@
 package scrabble.input;
 
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public interface InputCommand {
 
     boolean usesTurn();
 
     static InputCommand valueOf(String str) {
-        if (str.startsWith("EXCHANGE")) {
-            return ExchangeCommand.valueOf(str);
-        } else {
-            return PlaceCommand.valueOf(str);
-        }
+        Stream<Function<String, InputCommand>> parsers =
+                Stream.of(ExchangeCommand::valueOf, HelpCommand::valueOf, PlaceCommand::valueOf);
+
+        return parsers.map(
+                        parser -> {
+                            try {
+                                return parser.apply(str);
+                            } catch (IllegalArgumentException e) {
+                                return null;
+                            }
+                        })
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
     }
 }
