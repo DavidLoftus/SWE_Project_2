a463bf20bf60470b6ddf140791a03f8bdfb477bd
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-03-20 15:24:04 +0000

    added constructor to ExchangeCommand

--- src/main/java/scrabble/input/ExchangeCommand.java
+++ src/main/java/scrabble/input/ExchangeCommand.java
@@ -1,5 +1,6 @@
 package scrabble.input;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 import scrabble.Tile;
@@ -8,19 +9,26 @@ public class ExchangeCommand implements InputCommand {
 
     public List<Tile> tiles;
 
+    public ExchangeCommand(List<Tile> tiles) {
+    	tiles = tiles;
+    }
+    
     @Override
     public boolean usesTurn() {
         return true;
     }
 
     static ExchangeCommand valueOf(String str) {
-        str.toUpperCase(); // not sure if input has to be in upper case to be accepted or???
+        str = str.toUpperCase(); // not sure if input has to be in upper case to be accepted or???
         StringTokenizer string = new StringTokenizer(str, " ");
         if (!string.nextToken().equals("EXCHANGE")) {
             return null;
         }
-        List<Tile> tilesToReturn = null;
+        List<Tile> tilesToReturn = new ArrayList<>();
         char[] tileList = string.nextToken().toCharArray();
+        if(!string.hasMoreTokens()) {
+        	return null;
+        }
         for (int i = 0; i < tileList.length; i++) {
             for (Tile t : Tile.values()) {
                 if (tileList[i] == t.getLetter()) {
@@ -28,6 +36,7 @@ public class ExchangeCommand implements InputCommand {
                 }
             }
         }
-        return (ExchangeCommand) tilesToReturn;
+        return  new ExchangeCommand(tilesToReturn);
     }
+
 }
