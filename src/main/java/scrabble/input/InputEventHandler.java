f0fa106c0cf31feba001ab36195b577ac67e6a98
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-03-23 15:01:24 +0000

    oneTimeListeners for InputEventHandler

--- src/main/java/scrabble/input/InputEventHandler.java
+++ src/main/java/scrabble/input/InputEventHandler.java
@@ -6,17 +6,29 @@ import java.util.List;
 public class InputEventHandler implements InputListener {
 
     private List<InputListener> listeners = new ArrayList<>();
+    private List<InputListener> oneTimeListeners = new ArrayList<>();
 
     public void addListener(InputListener listener) {
         listeners.add(listener);
     }
 
+    public void addOneTimeListener(InputListener listener) {
+        oneTimeListeners.add(listener);
+    }
+
     public void removeListener(InputListener listener) {
         listeners.remove(listener);
+        oneTimeListeners.remove(listener);
     }
 
     @Override
     public void accept(String input) {
         listeners.forEach(listener -> listener.accept(input));
+
+        int len = oneTimeListeners.size();
+        for (int i = 0; i < len; ++i) {
+            oneTimeListeners.get(i).accept(input);
+        }
+        oneTimeListeners.subList(0, len).clear();
     }
 }
