c230d9c3f5e3766e0672257a82ff724c200900ee
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-03-23 19:37:03 +0000

    moving from wordPacement to wordRange

--- src/main/java/scrabble/WordPlacement.java
+++ src/main/java/scrabble/WordPlacement.java
@@ -16,35 +16,20 @@ public class WordPlacement {
         this.direction = direction;
         this.word = word.toUpperCase();
 
-        if (direction == Direction.HORIZONTAL) {
-            if (startPos.getColumn() + length() > 15) {
-                throw new IllegalArgumentException("Word extends outside bounds of Board");
-            }
-        } else {
-            if (startPos.getRow() + length() > 15) {
-                throw new IllegalArgumentException("Word extends outside bounds of Board");
-            }
-        }
+        new WordRange(startPos, direction, word.length());
     }
-
+    
     /** @return the length of the String word */
     public int length() {
         return word.length();
     }
-
-    /**
-     * @param i the index of the letter
-     * @return the row in which selected letter is in
-     */
-    public BoardPos getPositionAt(int i) {
-        if (i < 0 || i >= length()) {
-            throw new IndexOutOfBoundsException();
-        }
-        if (direction == Direction.HORIZONTAL) {
-            return new BoardPos(startPos.getRow(), startPos.getColumn() + i);
-        } else {
-            return new BoardPos(startPos.getRow() + i, startPos.getColumn());
-        }
+    
+    public BoardPos getStartPos() {
+    	return this.startPos;
+    }
+    
+    public Direction getDirection() {
+    	return this.direction;
     }
 
     /**
@@ -53,45 +38,6 @@ public class WordPlacement {
      */
     public char getLetterAt(int i) {
         return word.charAt(i);
-    }
-
-    private boolean hasTileAtIfValidPos(Board board, int i, int j) {
-        if (i < 0 || j < 0 || i >= 15 || j >= 15) {
-            return false;
-        }
-        return board.hasTileAt(new BoardPos(i, j));
-    }
-
-    public boolean isConnectedToExistingTile(Board board) {
-        for (int i = 0; i < length(); ++i) {
-            BoardPos pos = getPositionAt(i);
-
-            if (board.hasTileAt(pos)) {
-                return true;
-            }
-
-            int row = pos.getRow(), column = pos.getColumn();
-
-            if (hasTileAtIfValidPos(board, row - 1, column)
-                    || hasTileAtIfValidPos(board, row + 1, column)
-                    || hasTileAtIfValidPos(board, row, column - 1)
-                    || hasTileAtIfValidPos(board, row, column + 1)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean isPlacedAtStar(Board board) {
-        for (int i = 0; i < length(); ++i) {
-            BoardPos pos = getPositionAt(i);
-
-            if (board.getModiferAt(pos) == Square.Modifier.STAR) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
--- src/main/java/scrabble/WordRange.java
+++ src/main/java/scrabble/WordRange.java
@@ -1,8 +1,85 @@
 package scrabble;
 
+import scrabble.WordPlacement.Direction;
+
 /// TODO: This would essentially be copypaste of WordPlacement, fix up and update WordPlacement to use WordRange
 // ^ might involve fixing tests
 public class WordRange {
 
-    /// TODO: iterate function
+    private BoardPos startPos;
+    private Direction direction;
+    private int length;
+
+    public WordRange(BoardPos startPos, Direction direction, int lenght) {
+        this.startPos = startPos;
+        this.direction = direction;
+        this.length = lenght;
+
+        if (direction == Direction.HORIZONTAL) {
+            if (startPos.getColumn() + length > 15) {
+                throw new IllegalArgumentException("Word extends outside bounds of Board");
+            }
+        } else {
+            if (startPos.getRow() + length > 15) {
+                throw new IllegalArgumentException("Word extends outside bounds of Board");
+            }
+        }
+    }
+
+    /**
+     * @param i the index of the letter
+     * @return the row in which selected letter is in
+     */
+    public BoardPos getPositionAt(int i) {
+        if (i < 0 || i >= length) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (direction == Direction.HORIZONTAL) {
+            return new BoardPos(startPos.getRow(), startPos.getColumn() + i);
+        } else {
+            return new BoardPos(startPos.getRow() + i, startPos.getColumn());
+        }
+    }
+
+
+    private boolean hasTileAtIfValidPos(Board board, int i, int j) {
+        if (i < 0 || j < 0 || i >= 15 || j >= 15) {
+            return false;
+        }
+        return board.hasTileAt(new BoardPos(i, j));
+    }
+
+    public boolean isConnectedToExistingTile(Board board) {
+        for (int i = 0; i < length; ++i) {
+            BoardPos pos = getPositionAt(i);
+
+            if (board.hasTileAt(pos)) {
+                return true;
+            }
+
+            int row = pos.getRow(), column = pos.getColumn();
+
+            if (hasTileAtIfValidPos(board, row - 1, column)
+                    || hasTileAtIfValidPos(board, row + 1, column)
+                    || hasTileAtIfValidPos(board, row, column - 1)
+                    || hasTileAtIfValidPos(board, row, column + 1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isPlacedAtStar(Board board) {
+        for (int i = 0; i < length; ++i) {
+            BoardPos pos = getPositionAt(i);
+
+            if (board.getModiferAt(pos) == Square.Modifier.STAR) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
 }
