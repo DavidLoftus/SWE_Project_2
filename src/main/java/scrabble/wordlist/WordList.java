79184f5ab822c3f042e93ace9d8f14cffd5a7908
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-03-31 14:53:19 +0100

    WordList skeleton

---
+++ src/main/java/scrabble/wordlist/WordList.java
@@ -0,0 +1,20 @@
+package scrabble.wordlist;
+
+import java.io.InputStream;
+import java.util.Set;
+
+public class WordList {
+
+    public WordList() {
+        throw new UnsupportedOperationException();
+    }
+
+    private Set<String> readWordList(InputStream inputStream) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isValidWord(String word) {
+        throw new UnsupportedOperationException();
+    }
+
+}
