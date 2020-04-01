4ec18fdebf9a75bf9096acc8326f791773403a87
eadaoinmccavana <eadaoin.mccavana@ucdconnect.ie>
2020-04-01 16:03:54 +0100

    wordList

--- src/main/java/scrabble/wordlist/WordList.java
+++ src/main/java/scrabble/wordlist/WordList.java
@@ -1,20 +1,36 @@
 package scrabble.wordlist;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.zip.GZIPInputStream;
 
 public class WordList {
 
+    private Set<String> wordList;
+
     public WordList() {
-        throw new UnsupportedOperationException();
+        InputStream fileStream = WordList.class.getResourceAsStream("sowpods.txt.gz");
+        try {
+            this.wordList = readWordList(new GZIPInputStream(fileStream));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Set<String> readWordList(InputStream inputStream) {
-        throw new UnsupportedOperationException();
+        Set<String> wordList = new HashSet<>();
+        try (Scanner scanner = new Scanner(inputStream)) {
+            while (scanner.hasNext()) {
+                wordList.add(scanner.next());
+            }
+        }
+        return wordList;
     }
 
     public boolean isValidWord(String word) {
-        throw new UnsupportedOperationException();
+        return wordList.contains(word);
     }
-
 }
