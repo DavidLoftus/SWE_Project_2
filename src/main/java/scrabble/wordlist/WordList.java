1a5e35b6546ec9e35c6b9f806d50c514c55d7706
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2020-04-05 23:29:46 +0100

    WordList docs

--- src/main/java/scrabble/wordlist/WordList.java
+++ src/main/java/scrabble/wordlist/WordList.java
@@ -7,10 +7,18 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
+/**
+ * WordList holds a set of all valid words according to the SOWPODS scrabble dictionary. This
+ * dictionary is stored as a resource in the jar and is loaded during construction of the class.
+ */
 public class WordList {
 
     private Set<String> wordList;
 
+    /**
+     * Constructs the WordList by loading the sowpods.txt.gz file from resources and filling
+     * `wordList` with contents.
+     */
     public WordList() {
         InputStream fileStream = WordList.class.getResourceAsStream("sowpods.txt.gz");
         try {
@@ -20,6 +28,12 @@ public class WordList {
         }
     }
 
+    /**
+     * Reads word list from input stream to produce a set of words.
+     *
+     * @param inputStream decompressed input stream containing lines of dictionary words
+     * @return set containing all words from `inputStream`.
+     */
     private Set<String> readWordList(InputStream inputStream) {
         Set<String> wordList = new HashSet<>();
         try (Scanner scanner = new Scanner(inputStream)) {
@@ -30,6 +44,12 @@ public class WordList {
         return wordList;
     }
 
+    /**
+     * Checks if the given word is valid. This function is case insensitive.
+     *
+     * @param word to query for
+     * @return true if word is in dictionary otherwise false.
+     */
     public boolean isValidWord(String word) {
         return wordList.contains(word.toUpperCase());
     }
