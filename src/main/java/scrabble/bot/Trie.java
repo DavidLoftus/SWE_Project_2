7193f1f8ea1d27ec7133ba35db590ca098ec77fd
jayengracio <john.engracio@ucdconnect.ie>
2020-04-13 17:32:33 +0100

    Trie add arc code

--- src/main/java/scrabble/bot/Trie.java
+++ src/main/java/scrabble/bot/Trie.java
@@ -10,7 +10,22 @@ public class Trie implements Iterable<String> {
         }
     }
 
-    public Trie() {}
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz+";
+
+    private Trie[] children = new Trie[ALPHABET.length()];
+    private boolean isEnd = false;
+
+    private static int charToIndex(char c) {
+        int i = ALPHABET.indexOf(c);
+        if (i == -1) {
+            throw new IllegalArgumentException("invalid char: " + c);
+        }
+        return i;
+    }
+
+    private static char indexToChar(int i) {
+        return ALPHABET.charAt(i);
+    }
 
     public Trie add(String s) {
         // TODO: implement
@@ -18,18 +33,30 @@ public class Trie implements Iterable<String> {
     }
 
     public Trie addArc(char c) {
-        // TODO: implement
-        throw new UnsupportedOperationException();
+        int i = charToIndex(c);
+        if (children[i] == null) {
+            children[i] = new Trie();
+        }
+        return children[i];
     }
 
-    public Trie addFinalArc() {
-        // TODO: implement
-        throw new UnsupportedOperationException();
+    public Trie addFinalArc(char c1, char c2) {
+        // Run assertion on c2 before mutating trie to ensure operation is transactional.
+        charToIndex(c2);
+        Trie subTrie = addArc(c1);
+        Trie nestedSubTrie = subTrie.addArc(c2);
+        nestedSubTrie.isEnd = true;
+        return subTrie;
     }
 
     public Trie forceArc(char c, Trie trie) throws BadForceException {
-        // TODO: implement
-        throw new UnsupportedOperationException();
+        int i = charToIndex(c);
+        if (children[i] == null) {
+            children[i] = trie;
+        } else if (children[i] != trie) {
+            throw new BadForceException("Tried to force arc '" + c + "' but node already exists.");
+        }
+        return children[i];
     }
 
     public Trie get(char c) {
