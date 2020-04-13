dfbbf7ed6bc6d8272f0fd200aeaf4412a82f2af2
jayengracio <john.engracio@ucdconnect.ie>
2020-04-13 17:11:08 +0100

    Trie tests

---
+++ src/test/java/scrabble/bot/TrieTest.java
@@ -0,0 +1,112 @@
+package scrabble.bot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrieTest {
+
+    private Trie trie;
+
+    @BeforeEach
+    void init() {
+        this.trie = new Trie();
+    }
+
+    @Test
+    void add() {
+        trie.add("helloworld");
+        trie.add("hello+world");
+
+        assertThrows(IllegalArgumentException.class, () -> trie.add("hello world"));
+        assertThrows(IllegalArgumentException.class, () -> trie.add("helloworld!"));
+    }
+
+    @Test
+    void addArc() {
+    }
+
+    @Test
+    void addFinalArc() {
+    }
+
+    @Test
+    void forceArc() {
+    }
+
+    @Test
+    void get() {
+        trie.add("helloworld");
+        trie.add("hello+world");
+
+        Trie subTrie = trie.get("hello");
+        assertThrows(NoSuchElementException.class, () -> trie.get("baby"));
+        assertThrows(IllegalArgumentException.class, () -> trie.get("hello "));
+
+        assertNotNull(subTrie.get("worl"));
+        assertNotNull(subTrie.get("+worl"));
+
+        assertThrows(NoSuchElementException.class, () -> subTrie.get("baby"));
+    }
+
+    @Test
+    void contains() {
+        trie.add("helloworld");
+        trie.add("hello+world");
+
+        Trie subTrie = trie.get("hello");
+
+        assertTrue(subTrie.contains("world"));
+        assertTrue(subTrie.contains("+world"));
+
+        assertFalse(subTrie.contains("worl"));
+        assertFalse(subTrie.contains("+worl"));
+
+        assertFalse(trie.contains("goodbyeworld"));
+    }
+
+    @Test
+    void iterator() {
+        trie.add("helloworld");
+        trie.add("hello+world");
+
+        {
+            Iterator<String> iter = trie.iterator();
+
+            assertTrue(iter.hasNext());
+            assertEquals("hello+world", iter.next());
+
+            assertTrue(iter.hasNext());
+            assertEquals("helloworld", iter.next());
+
+            assertFalse(iter.hasNext());
+        }
+
+        {
+            Trie subTrie = trie.get("hello");
+            Iterator<String> iter = trie.iterator();
+
+            assertTrue(iter.hasNext());
+            assertEquals("+world", iter.next());
+
+            assertTrue(iter.hasNext());
+            assertEquals("world", iter.next());
+
+            assertFalse(iter.hasNext());
+        }
+
+        {
+            Trie subTrie = trie.get("hello+");
+            Iterator<String> iter = trie.iterator();
+
+            assertTrue(iter.hasNext());
+            assertEquals("world", iter.next());
+
+            assertFalse(iter.hasNext());
+        }
+    }
+}
